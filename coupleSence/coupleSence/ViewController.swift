15a098b38ca2b17e18027a857984bdfb28b5ed8c
jeonggeunsoo <jeonggeunsoo@jeonggeunsooui-MacBook-Pro.local>
2017-05-28 15:03:05 +0900

    test commit

---
+++ coupleSence/coupleSence/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coupleSence
+//
+//  Created by jeonggeunsoo on 2017. 5. 28..
+//  Copyright © 2017년 jeonggeunsoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
